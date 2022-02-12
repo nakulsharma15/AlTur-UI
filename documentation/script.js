b664d8b6c490747a1848150d8e428c5e8b52a760
nakulsharma15 <nakuls309@gmail.com>
2022-02-12 23:24:55 +0530

    feat: added documentation

---
+++ documentation/script.js
@@ -0,0 +1,7 @@
+hljs.highlightAll();
+let codeBlocks = document.querySelectorAll("pre code");
+codeBlocks.forEach((codeBlock) => {
+    let code = codeBlock.innerHTML;
+    code = code.replaceAll("<", "&lt;");
+    codeBlock.innerHTML = code;
+});
